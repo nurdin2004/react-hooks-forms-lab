a5e8e255d2336d8d9792e0f6ed3bab0fffbf1d46
nurdin2004 <adiaderkisohouf@gmail.com>
2025-04-24 14:26:25 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,17 @@ import Header from "./Header";
 import itemData from "../data/items";
 
 function App() {
-  const [items, setItems] = useState(itemData);
+  const [items, setItems] = useState(itemData); // Make sure to add setItems here
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+    setIsDarkMode((prevMode) => !prevMode);
   }
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} setItems={setItems} /> {/* Pass setItems here */}
     </div>
   );
 }
